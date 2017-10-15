9e33edd1ff9fba0225258889ee4859d0fdddbd83
starhaotian <starhaotian@gmail.com>
2017-10-14 20:20:40 -0700

    Candy

---
+++ assignment5/Candy.java
@@ -0,0 +1,27 @@
+package Hw_5;
+
+/**
+ * Created by starhaotian on 13/10/2017.
+ */
+public class Candy extends DessertItem{
+    private double weight;
+    private int price;
+
+    public Candy( String name, double weight, int price){
+        super(name);
+        this.weight = weight;
+        this.price = price;
+    }
+    public int getCost(){
+        return (int)(weight * price + 0.5f);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder builder=new StringBuilder();
+        builder.append(weight+" lbs. @ "+price+ " /lb.\n");
+        builder.append(super.toString());
+
+        return builder.toString();
+    }
+}
